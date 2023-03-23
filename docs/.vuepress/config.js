7a9a10bc3d1a101b85a278594fa4d5bc40136f77
maoxueba <1625660674@qq.com>
2023-03-23 14:13:04 +0800

    部署到github

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,27 @@
+const moment = require('moment');
+moment.locale("zh-cn");
+
 module.exports = {
-  title:"小茂哥哥的面试宝典",
-  description:"勇敢阿茂 不怕困难",
+  base:'/docs/',
+  title: "小茂哥哥的面试宝典",
+  description: "勇敢阿茂 不怕困难",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.icon' }],
-    ['meta', { name:'author', content:'小茂哥哥'}]
+    ["link", { rel: "icon", href: "/favicon.icon" }],
+    ["meta", { name: "author", content: "小茂哥哥" }],
+  ],
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          return moment(timestamp).format("lll");
+        }
+      }
+    ]
   ],
   themeConfig: {
     logo: "/assets/img/logo.jpg",
-    sidebar: 'auto',
+    sidebar: "auto",
     nav: [
       { text: "首页", link: "/" },
       { text: "关于", link: "/about" },
@@ -22,20 +36,17 @@ module.exports = {
       { text: "更多", link: "https://bing.com" },
     ],
     sidebar: [
-        '/',
-        '/about',
-        ['/more', '更多'],
-        {
-          title: 'CSS',   // 必要的
-          path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          collapsable: false, // 可选的, 默认值是 true,
-          sidebarDepth: 1,    // 可选的, 默认值是 1
-          children: [
-            '/css/css-1',
-            '/css/css-2',
-            '/css/css-3',
-          ]
-        }
-      ]
+      "/",
+      "/about",
+      ["/more", "更多"],
+      {
+        title: "CSS", // 必要的
+        path: "/css/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ["/css/css-1", "/css/css-2", "/css/css-3"],
+      },
+    ],
+    lastUpdated: '更新时间'
   },
 };
